a17b12abd72793756315dca4282f9d3ab70c6be8
andern7 <anderson.nicolette@gmail.com>
2015-04-01 10:30:52 -0500

    Create ContactModel.cs

---
+++ ContactModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
